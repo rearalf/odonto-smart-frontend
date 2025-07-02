bbd7ab7ffd7ace74f47b9ceeafb28ecff53b7917
Ricardo Alf <rear_recinos@hotmail.com>
2025-07-02 15:48:02 -0600

    refactor: Enhance LoadingProgress component

--- src/components/LoadingProgress/index.tsx
+++ src/components/LoadingProgress/index.tsx
@@ -1,14 +1,42 @@
-import { Box, CircularProgress, Fade } from '@mui/material';
-
+import { Box, CircularProgress, Fade, useTheme } from '@mui/material';
 import useLoadingStore from '../../stores/useLoadingStore';
-import './styles.css';
 
 const LoadingProgress = () => {
   const store = useLoadingStore();
+  const theme = useTheme();
+
+  if (!store.loading) return null;
+
   return (
     <Fade in={store.loading}>
-      <Box component="div" className="loading-progress">
-        <CircularProgress size={70} />
+      <Box
+        component="div"
+        role="status"
+        aria-live="polite"
+        sx={{
+          backgroundColor:
+            theme.palette.mode !== 'dark'
+              ? 'rgba(0, 0, 0, 0.6)'
+              : 'rgba(255, 255, 255, 0.6)',
+          backdropFilter: 'blur(4px)',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: 1300,
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <CircularProgress
+          size={70}
+          thickness={4}
+          sx={{
+            color: theme.palette.primary.main,
+          }}
+        />
       </Box>
     </Fade>
   );
