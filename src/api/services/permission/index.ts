48153f31b4bea56ff1cfdd89fb11ca8b216b8ecc
Ricardo Alf <rear_recinos@hotmail.com>
2025-07-07 15:06:40 -0600

    feat: Implement grouped permissions functionality in role management

--- src/api/services/permission/index.ts
+++ src/api/services/permission/index.ts
@@ -1,13 +1,28 @@
-import { axiosInstance } from '@api/axios/axiosInstance';
-import type { IBasicIdNameDescription } from 'src/types/common.types';
-import { handleError } from '@utils/handleError';
 import type { AxiosError } from 'axios';
+
 import permissionEndpoints from '@api/endpoints/permission';
+import { axiosInstance } from '@api/axios/axiosInstance';
+import { handleError } from '@utils/handleError';
+import type {
+  IPermissionGrouped,
+  IBasicIdNameDescription,
+} from 'src/types/common.types';
 
 export const getAllPermission = async () => {
   try {
     const response = await axiosInstance.get<IBasicIdNameDescription[]>(
       permissionEndpoints.getAllPermissions,
+    );
+    return response;
+  } catch (error) {
+    throw handleError(error as AxiosError<{ message?: string }>);
+  }
+};
+
+export const getAllPermissionGrouped = async () => {
+  try {
+    const response = await axiosInstance.get<IPermissionGrouped[]>(
+      permissionEndpoints.getAllPermissionsGrouped,
     );
     return response;
   } catch (error) {
